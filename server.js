a834eb44290fa20ba8d2c713205727c7a2512fc4
enmanuel <enmanuelguzman107123@gmail.com>
2022-01-06 08:16:22 -1100

    added more config

--- server.js
+++ server.js
@@ -32,3 +32,10 @@ process.on('unhandledRejection', (err) => {
     process.exit(1);
   });
 });
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM received, shutting down');
+  server.close(() => {
+    console.log('process terminated');
+  });
+});
