ecc579e402b1b6f9c0d3ee37913804c9dc12fb42
enmanuel <enmanuelguzman107123@gmail.com>
2022-01-07 10:51:51 -1100

    bugs fix

--- public/js/alert.js
+++ public/js/alert.js
@@ -7,5 +7,5 @@ export const showAlert = (type, msg, time = 7) => {
   hideAlert();
   const markup = `<div class="alert alert--${type}">${msg}</div>`;
   document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
-  window.setTimeout(hideAlert, time * 2000);
+  window.setTimeout(hideAlert, time * 1000);
 };
