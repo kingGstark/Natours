807ebb22d4bcecba2f9fc76d4e5f50c9c8d44f57
enmanuel <enmanuelguzman107123@gmail.com>
2022-01-07 09:37:43 -1100

    bugs fix

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -9,7 +9,10 @@ router.get('/checkout-session/:tourId', bookingController.getCheckoutSession);
 
 router.use(authController.restrictTo('admin', 'lead-guide'));
 
-router.route('/').get(bookingController.getAllBooking);
+router
+  .route('/')
+  .get(bookingController.getAllBooking)
+  .post(bookingController.createBooking);
 router
   .route('/:id')
   .get(bookingController.getBooking)
